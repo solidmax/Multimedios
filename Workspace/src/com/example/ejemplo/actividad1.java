65956969fdede687df3c217e743c166116fac7c8
Maximiliano Zamora <maxi.zamora@gmail.com>
2013-10-05 13:41:34 -0400

    ejemplo1

---
+++ Workspace/src/com/example/ejemplo/actividad1.java
@@ -0,0 +1,14 @@
+package com.example.ejemplo;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class actividad1 extends Activity {
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad1);
+     
+    }
+}
